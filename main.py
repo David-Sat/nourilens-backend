22ab5bfe75d58a4b7de13cce94d275694accbc3b
David Satomi <d.satomi@outlook.de>
2024-01-25 17:01:05 +0100

    Added Image processing

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from typing import Union
+from fastapi import FastAPI, File, UploadFile
+from io import BytesIO
+from PIL import Image
+from ai_utils.ai_adapter import process_image, filter_list, create_raw_json, enrich_json
+from ai_utils.failure_handling import retry_function
 
-from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.post("/upload")
+async def upload_image(file: UploadFile = File(...)):
+    # Read and process the image
+    image_bytes = await file.read()
+    image_stream = BytesIO(image_bytes)
+    pil_image = Image.open(image_stream)
+    processed_image_output = retry_function(process_image, pil_image)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+    # Filter, convert to JSON, and enrich the data
+    filtered_list = retry_function(filter_list, processed_image_output)
+    raw_json = retry_function(create_raw_json, filtered_list)
+    enriched_json = retry_function(enrich_json, raw_json)
 
+    return {"message": "Image processed and data enriched", "enriched_output": enriched_json}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
